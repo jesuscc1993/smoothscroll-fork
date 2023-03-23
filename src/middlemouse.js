33dec36bd3bed8e608622e905d1c654a3a3da001
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-03-23 21:00:15 +0100

    Fixed deprecated function usage

--- src/middlemouse.js
+++ src/middlemouse.js
@@ -1,8 +1,7 @@
-
 //
 // SmoothScroll (Balazs Galambosi)
 // Licensed under the terms of the MIT license.
-// The only restriction would be not to publish any  
+// The only restriction would be not to publish any
 // extension for browsers or native application
 // without getting a written permission first.
 //
@@ -11,170 +10,174 @@
  * A module for middle mouse scrolling.
  */
 (function (window) {
+  var defaultOptions = {
+    middleMouse: false,
+    frameRate: 200,
+  };
 
-var defaultOptions = {
-    middleMouse : false,
-    frameRate   : 200
-};
+  var options = defaultOptions;
 
-var options = defaultOptions;
+  var img = document.createElement('div'); // img at the reference point
+  var scrolling = false; // guards one phase
 
-var img = document.createElement("div"); // img at the reference point
-var scrolling = false; // guards one phase
+  // we check the OS for default middle mouse behavior only!
+  var isLinux = navigator.platform.indexOf('Linux') != -1;
 
-
-// we check the OS for default middle mouse behavior only!
-var isLinux = (navigator.platform.indexOf("Linux") != -1); 
-
-// get global settings
-chrome.storage.sync.get(defaultOptions, function (syncedOptions) {
+  // get global settings
+  chrome.storage.sync.get(defaultOptions, function (syncedOptions) {
     options = syncedOptions;
     // leave time for the main script to check excluded pages
-    setTimeout(function() {
-        // if we shouldn't run, stop listening to events
-        if (isExcluded && !options.middleMouse) {
-            cleanup();
-        }
+    setTimeout(function () {
+      // if we shouldn't run, stop listening to events
+      if (isExcluded && !options.middleMouse) {
+        cleanup();
+      }
     }, 10);
-});
+  });
 
- 
-/**
- * Initializes the image at the reference point.
- */
-function init() {
-    var url = chrome.extension.getURL("../img/cursor.png");
+  /**
+   * Initializes the image at the reference point.
+   */
+  function init() {
+    var url = chrome.runtime.getURL('../img/cursor.png');
     var style = img.style;
-    style.background = "url("+url+") no-repeat";
-    style.position   = "fixed";
-    style.zIndex     = "1000";
-    style.width      = "20px";
-    style.height     = "20px";
-    new Image().src  = url; // force download
-}
+    style.background = 'url(' + url + ') no-repeat';
+    style.position = 'fixed';
+    style.zIndex = '1000';
+    style.width = '20px';
+    style.height = '20px';
+    new Image().src = url; // force download
+  }
 
-/**
- * Removes event listeners and other traces left on the page.
- */
-function cleanup() {
-    removeEvent("mousedown", mousedown);
-}
+  /**
+   * Removes event listeners and other traces left on the page.
+   */
+  function cleanup() {
+    removeEvent('mousedown', mousedown);
+  }
 
-/**
- * Shows the reference image, and binds event listeners for scrolling.
- * It also manages the animation.
- * @param {Object} event
- */
-function mousedown(e) {
-
+  /**
+   * Shows the reference image, and binds event listeners for scrolling.
+   * It also manages the animation.
+   * @param {Object} event
+   */
+  function mousedown(e) {
     // use default action if we're disabled
     // or it's not the midde mouse button
     if (!options.middleMouse || e.button !== 1) {
-        return true;
+      return true;
     }
 
     var isLink = false;
-    var elem   = e.target;
-    
+    var elem = e.target;
+
     // linux middle mouse shouldn't be overwritten (paste)
-    var isLinuxInput = (isLinux && (/input|textarea/i.test(elem.nodeName) || 
-                                    elem.isContentEditable));
+    var isLinuxInput =
+      isLinux &&
+      (/input|textarea/i.test(elem.nodeName) || elem.isContentEditable);
 
     do {
-        isLink = isNodeName(elem, "a");
-        if (isLink) break;
+      isLink = isNodeName(elem, 'a');
+      if (isLink) break;
     } while ((elem = elem.parentNode));
-        
+
     elem = overflowingAncestor(e.target);
-    
+
     // if it's being used on an <a> element
     // take the default action
     if (!elem || isLink || isLinuxInput) {
-        return true;
+      return true;
     }
-    
+
     // we don't want the default by now
     e.preventDefault();
-    
+
     // quit if there's an ongoing scrolling
     if (scrolling) {
-        return false;
+      return false;
     }
-    
+
     // set up a new scrolling phase
     scrolling = true;
- 
+
     // reference point
-    img.style.left = e.clientX - 10 + "px";
-    img.style.top  = e.clientY - 10 + "px";
+    img.style.left = e.clientX - 10 + 'px';
+    img.style.top = e.clientY - 10 + 'px';
     document.body.appendChild(img);
-    
+
     var refereceX = e.clientX;
     var refereceY = e.clientY;
 
     var speedX = 0;
     var speedY = 0;
-    
+
     // animation loop
     var last = dateNow();
     var delay = 1000 / options.frameRate;
     var finished = false;
-    
-    window.requestAnimationFrame(function step(time) {
+
+    window.requestAnimationFrame(
+      function step(time) {
         var now = dateNow();
         var elapsed = now - last;
         // NOTE: later can also use document.scrollingElement
         if (elem == document.body) {
-            window.scrollBy(speedX * elapsed, speedY * elapsed);
+          window.scrollBy(speedX * elapsed, speedY * elapsed);
         } else {
-            elem.scrollLeft += (speedX * elapsed) >> 0;
-            elem.scrollTop  += (speedY * elapsed) >> 0;
+          elem.scrollLeft += (speedX * elapsed) >> 0;
+          elem.scrollTop += (speedY * elapsed) >> 0;
         }
         last = now;
         if (!finished) {
-            window.requestAnimationFrame(step);
+          window.requestAnimationFrame(step);
         }
-    }, elem, delay);
-    
+      },
+      elem,
+      delay
+    );
+
     var firstMove = true;
 
     function mousemove(e) {
-        var deltaX = Math.abs(refereceX - e.clientX);
-        var deltaY = Math.abs(refereceY - e.clientY);
-        var movedEnough = Math.max(deltaX, deltaY) > 10; 
-        if (firstMove && movedEnough) {
-            addEvent("mouseup", remove);
-            firstMove = false;
-        }
-        speedX = (e.clientX - refereceX) * 10 / 1000;
-        speedY = (e.clientY - refereceY) * 10 / 1000;
+      var deltaX = Math.abs(refereceX - e.clientX);
+      var deltaY = Math.abs(refereceY - e.clientY);
+      var movedEnough = Math.max(deltaX, deltaY) > 10;
+      if (firstMove && movedEnough) {
+        addEvent('mouseup', remove);
+        firstMove = false;
+      }
+      speedX = ((e.clientX - refereceX) * 10) / 1000;
+      speedY = ((e.clientY - refereceY) * 10) / 1000;
     }
-    
+
     function remove(e) {
-        removeEvent("mousemove", mousemove);
-        removeEvent("mousedown", remove);
-        removeEvent("mouseup", remove);
-        removeEvent("keydown", remove);
-        document.body.removeChild(img);
-        scrolling = false;
-        finished  = true;
+      removeEvent('mousemove', mousemove);
+      removeEvent('mousedown', remove);
+      removeEvent('mouseup', remove);
+      removeEvent('keydown', remove);
+      document.body.removeChild(img);
+      scrolling = false;
+      finished = true;
     }
-    
-    addEvent("mousemove", mousemove);
-    addEvent("mousedown", remove);
-    addEvent("keydown", remove);
-}
 
-/**
- * performance.now with fallback
- */
-var dateNow = (function () {
-  return (window.performance && performance.now) 
-        ? function () { return performance.now(); }
-        : function () { return Date.now(); };
-})();
+    addEvent('mousemove', mousemove);
+    addEvent('mousedown', remove);
+    addEvent('keydown', remove);
+  }
 
-addEvent("mousedown", mousedown);
-addEvent("DOMContentLoaded", init);
+  /**
+   * performance.now with fallback
+   */
+  var dateNow = (function () {
+    return window.performance && performance.now
+      ? function () {
+          return performance.now();
+        }
+      : function () {
+          return Date.now();
+        };
+  })();
 
+  addEvent('mousedown', mousedown);
+  addEvent('DOMContentLoaded', init);
 })(window);
